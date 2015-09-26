17a8d4a2fd1c02de5fe2a60d0e9230e4f4fc3be5
Roberto Carlos Gonzalez Flores <iscrobertogonzalez@gmail.com>
2015-09-26 10:32:20 -0700

    Setting up the UnitTest environment for the java module

---
+++ weather-data-provider/src/test/java/org/rcgonzalezf/weather/WeatherRepositoryTest.java
@@ -0,0 +1,17 @@
+package org.rcgonzalezf.weather;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(JUnit4.class)
+public class WeatherRepositoryTest {
+
+  @Test
+  public void sumOk() {
+    assertEquals(2, 1+1);
+  }
+
+}
