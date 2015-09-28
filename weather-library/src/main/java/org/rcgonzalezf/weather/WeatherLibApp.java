90f5323b0993d046a3147c412c6ecf40ccd8c191
Roberto Carlos Gonzalez Flores <iscrobertogonzalez@gmail.com>
2015-09-27 22:52:15 -0700

    Enabling HTTP cache on file storage

--- weather-library/src/main/java/org/rcgonzalezf/weather/WeatherLibApp.java
+++ weather-library/src/main/java/org/rcgonzalezf/weather/WeatherLibApp.java
@@ -1,10 +1,14 @@
 package org.rcgonzalezf.weather;
 
 import android.app.Application;
+import android.util.Log;
+import java.io.File;
 import org.rcgonzalezf.weather.common.ServiceConfig;
 import org.rcgonzalezf.weather.common.WeatherProvider;
 
 public class WeatherLibApp extends Application {
+
+  private static final String TAG = WeatherLibApp.class.getSimpleName();
 
   private ServiceConfig mServiceConfig;
   private static WeatherLibApp sAppInstance = null;
@@ -14,6 +18,8 @@ public class WeatherLibApp extends Application {
     mServiceConfig = ServiceConfig.getInstance();
     mServiceConfig.setApiKey(getString(R.string.open_weather_map_api_key));
     mServiceConfig.setWeatherProvider(WeatherProvider.OpenWeather);
+
+    enableHttpResponseCache();
   }
 
   public static WeatherLibApp getInstance() {
@@ -22,6 +28,18 @@ public class WeatherLibApp extends Application {
 
   protected static void setAppInstance(WeatherLibApp appInstance) {
     sAppInstance = appInstance;
+  }
+
+  private void enableHttpResponseCache() {
+    try {
+      long httpCacheSize = 10 * 1024 * 1024; // 10 MiB
+      File httpCacheDir = new File(getCacheDir(), "http");
+      Class.forName("android.net.http.HttpResponseCache")
+          .getMethod("install", File.class, long.class)
+          .invoke(null, httpCacheDir, httpCacheSize);
+    } catch (Exception httpResponseCacheNotAvailable) {
+      Log.d(TAG, "HTTP response cache is unavailable.");
+    }
   }
 
 
